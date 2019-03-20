69620eec6bc2421407aa59b3d3a7fd7290ab9635
orduek <orduek@gmail.com>
2019-03-20 11:38:15 -0400

    Added basic file manipulation

--- extractingKPE_taskdata.py
+++ extractingKPE_taskdata.py
@@ -12,10 +12,10 @@ import bioread
 import os
 
 # loading file
-#a= bioread.read('/media/Drobo/Levy_Lab/Projects/PTSD_KPE/physio_data/raw/kpe1387/scan_1/kpe1387.1_scripts_2018-09-10T08_39_24.acq')
+#a= bioread.read('/media/Drobo/Levy_Lab/Projects/PTSD_KPE/physio_data/raw/kpe1403/Scan_4/KPE1403.4_task_2018-09-12T12_41_45.acq')
 
 # choose scripts channel
-#b = a.channels[7].raw_data
+#b = a.named_channels["Script"].raw_data
 #%%
 # create loop that will look for changes between zero and back to zero as on and off set time points. 
 def lookZero(b, offSet): # take Channel data and if we need to adjust timings
@@ -41,6 +41,7 @@ matlabwriter.MatlabWriter.write_file(a, "myfile.mat")
 def kpeTaskDat(filename):
     # takes filename and returns data frame of onsets and duration. Needs to attach condition and subject number
     import pandas as pd
+    import bioread
     a = bioread.read(filename)
    ## Take the first ready screen
     readyScreen = a.named_channels["Ready Screen"].raw_data
@@ -52,11 +53,12 @@ def kpeTaskDat(filename):
     b = a.named_channels["Script"].raw_data
     scriptTime = lookZero(b, diff)
     duration = []
-    condition = []
+ 
     for i in range(len(scriptTime[0])): # run through the set
         duration.append(round(scriptTime[1][i] - scriptTime[0][i])) # create duration
     events= pd.DataFrame({'onset':scriptTime[0], 'duration':duration})
     
+    return events
 
 
 #%% This part takes the scan sheet and create a data frame with condition and sessions. 
@@ -77,6 +79,8 @@ for index,rows  in totalScanData.iterrows():
 trialOrder = pd.DataFrame({'subject_id': totalScanData["subject_id"], "scriptOrder":totalScanData["Script Order"], "session":totalScanData["scan_num"]})
  # read subject id and pick the right line from the data frame
 
+
+
 subjectId = "kpe" + str(subNum)
 subjectData = trialOrder[trialOrder.subject_id==subjectId]
 # rnu through all session of subject and create conditions with onset and duration. 
@@ -93,7 +97,7 @@ for s , r in subjectData.iterrows(): # s is index and r is the actual row
             condition.append('trauma')
         else:
             pass
-                    
+    
     events= pd.DataFrame({'onset':scriptTime[0], 'duration':duration, 'condition':condition})
     # now we should create a data frame
     
@@ -101,3 +105,82 @@ for s , r in subjectData.iterrows(): # s is index and r is the actual row
                
     return (events)
             
+#%% Get all acq files
+import glob
+# get all scripts acq files
+fileList = glob.glob('/media/Drobo/Levy_Lab/Projects/PTSD_KPE/physio_data/raw/kpe*/**/*scripts*.acq')
+# remove faulty script file PLEASE MAKE SURE THE LOCATION WAS NOT CHANGED
+del fileList[16]
+
+def getSubjectNum(filePath):
+    # simple function to get subject number from data
+    a = filePath.split('/kpe')
+    b = a[1].split('/')
+    subjectNum = b[0]
+    return subjectNum
+
+def getScanNum(filePath):
+    # simple function to get scan number from file name
+    a= filePath.split('can_')
+    b = a[1].split('/')
+    scanNum = b[0]
+    return int(scanNum)
+
+#%%
+# now create a loop through all files
+import numpy as np
+events = []   
+totalEvents = [] #np.array([])
+# get subject number + name
+for file in fileList:
+    print(file)
+    subNum = getSubjectNum(file)
+    subjectId = "kpe" + str(subNum)
+    scanNum = getScanNum(file)
+    subjectData = trialOrder[trialOrder.subject_id==subjectId][trialOrder.session == scanNum] #, trialOrder.session==scanNum]
+   # sessionData = subjectData[subjectData.session == scanNum]
+    # rnu through all session of subject and create conditions with onset and duration. 
+    condition = []
+    for s , r in subjectData.iterrows(): # s is index and r is the actual row
+        print(s)
+        breakTrial = subjectData["scriptOrder"][s].split(';') # now its the first line but should be with subject id accordinaly.  Removed the comments
+        breakTrial2 = breakTrial[0].split()         
+        
+        for n in breakTrial2:
+            print (n)
+            if 'Sad' in n:
+                condition.append('sad')
+            elif 'Relax' in n:
+                condition.append('relax')
+            elif 'Relaxing' in n:
+                condition.append('relax')
+            elif 'Trauma' in n:
+                condition.append('trauma')
+            else:
+                pass
+    events = kpeTaskDat(file)
+    events["condition"] = condition
+    events["session"] = scanNum
+    events["subject"] = subNum
+    totalEvents.insert(int(subNum), events)
+   # totalEvents = np.append(totalEvents, events)
+type(totalEvents)
+totalEvents[8]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
