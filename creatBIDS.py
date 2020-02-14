0ae9c7aa8e33ad4a7324cbbfe46e7a928552efea
orduek <orduek@gmail.com>
2020-02-14 10:56:04 -0500

    Adding change of names to aging project

--- creatBIDS.py
+++ creatBIDS.py
@@ -93,14 +93,14 @@ def organizeFiles(output_dir, subName, session):
 #%%
 sessionDict = {
 
-      'ses-1': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF020/RCF020_D1_tb1506_harpaz-rotem',
-'ses-2': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF020/RCF020_D2_tb1515_harpaz-rotem',
+      'ses-1': '/media/Data/Aging/Raw_Data/AG_15/pb10255_levy',
+#'ses-2': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF020/RCF020_D2_tb1515_harpaz-rotem',
 #'ses-3': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF020/RCF020_D3_tb1521_harpaz-rotem',
 #'ses-4': '/media/Data/PTSD_KPE/kpe1468/kpe1468_scan4_pb9179_harpaz-rotem'
         }
-subNumber = '020'
+subNumber = '015'
 def fullBids(subNumber, sessionDict):
-    output_dir = '/media/Data/work/'
+    output_dir = '/media/Data/Aging/agingBIDS'
     subName = 'sub-' + subNumber
   #  folder_name = ['anat','func','dwi','other']
     
@@ -116,4 +116,44 @@ def fullBids(subNumber, sessionDict):
     
     #print (v)
 #%%
-#fullBids(subNumber, sessionDict)
+fullBids(subNumber, sessionDict)
+
+#%%
+import glob
+import json
+
+ses = {}
+root_dir='/media/Data/Aging/AgingBIDS/'
+file_structure = '/ses-1/func/sub-*_ses-1*.json'
+#glober = root_dir+file_stracture
+
+glober = root_dir+"sub-"+subNumber+file_structure
+# get all subjects data in a dicionary
+for sub in glob.glob(glober):
+    name = sub.split(sep="/")
+    if name[5] not in ses:
+        ses[name[5]] = {}
+    if "rest" not in sub:  
+        with open(sub, "r") as read_file:
+            data = json.load(read_file)
+            name = sub.split(sep="/")
+            ses[name[5]][name[8].split("_")[2]]=data['SeriesNumber']    
+
+# rearange order and set it to start at 1
+for key in ses:
+    sub = ses[key]
+    key_min = min(sub.keys(), key=(lambda k: sub[k]))
+    value_min = sub[key_min]
+    for k in sub:
+        sub[k]=sub[k]-value_min+1
+        
+for key in ses:
+    sub =ses[key]
+    for task in sub:
+        oldnifti = root_dir+key+'/ses-1/func/'+key+"_ses-1_"+task+"_bold.nii.gz"
+        newnifti = root_dir+key+'/ses-1/func/'+key+"_ses-1_task-"+str(sub[task])+"_bold.nii.gz"
+               
+        oldjson = root_dir+key+'/ses-1/func/'+key+"_ses-1_"+task+"_bold.json"
+        newjson = root_dir+key+'/ses-1/func/'+key+"_ses-1_task-"+str(sub[task])+"_bold.json"
+        os.rename(oldnifti,newnifti)
+        os.rename(oldjson,newjson)
