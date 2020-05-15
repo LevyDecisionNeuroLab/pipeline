0719a6ed76beef709d5fff63c70334fed3c51bd6
KoremNSN <40990514+KoremNSN@users.noreply.github.com>
2020-05-15 10:18:56 -0400

    Works with task!
    output DWI as runs - 4 files per DWI scan
    task output - if no task name is available 'task-task#' based on rank of series number
    if task name available 'task-taskName'

--- createBIDS_mul_dti.py
+++ createBIDS_mul_dti.py
@@ -46,17 +46,22 @@ def checkTask(funcdir, subName, ses):
     
     for f in files:
         if f.find('rest')!=-1:
-            task['rest'][f.split('_')[1]] = len(task['rest'])
+            task['rest'][f.split('_')[1]] = 'rest'+str(len(task['rest']))
         else:
-            task['task'][f.split('_')[1]] = len(task['task'])
+            run = f.split('bold')[1].split('.')[0].replace('(MB4iPAT2)','').replace('task','').replace('_','').replace('-','')
+            if  run == '':
+                task['task'][f.split('_')[1]] = 'task'+str(len(task['task']))
+            else:
+                task['task'][f.split('_')[1]] = run
+                            
     
     for f in files:
         ext = checkGz(os.path.splitext(f))
         src = (os.path.join(funcdir, f))
         kind = 'task'
         if f.find('rest')!=-1:
-            kind = 'rest'
-        dest = funcdir + subName + '_' + ses + '_task-' + kind + str(task[kind][f.split('_')[1]]) + "_bold" + ext
+             kind = 'rest'
+        dest = funcdir + subName + '_' + ses + '_task-' + task[kind][f.split('_')[1]] + "_bold" + ext
         
         os.rename(src, dest)
 
@@ -84,7 +89,7 @@ def organizeFiles(output_dir, subName, session):
     os.makedirs(fullPath + '/misc')    
     
     a = next(os.walk(fullPath)) # list the subfolders under subject name
-
+    dwi = False
     # run through the possibilities and match directory with scan number (day)
     for n in a[2]:
         print (n)
@@ -92,6 +97,7 @@ def organizeFiles(output_dir, subName, session):
         # add method to find (MB**) in filename and scrape it
         if n.find('diff')!=-1:
             print ('This file is DWI')
+            dwi = True
             shutil.move((fullPath +'/' + n), fullPath + '/dwi/' + n)
         elif n.find('MPRAGE')!=-1:
             print (n + ' Is Anat')
@@ -117,17 +123,19 @@ def organizeFiles(output_dir, subName, session):
             shutil.move((fullPath + '/' + n), (fullPath + '/misc/' + n))
            
     checkTask((fullPath + '/func/'), subName, session)
-    checkdwi((fullPath + '/dwi/'), subName, session)
+    if dwi:
+        checkdwi((fullPath + '/dwi/'), subName, session)
     
 # need to run thorugh misc folder and extract t1's when there is no MPRAGE - Need to solve issue with t1 - as adding the names is not validated with BIDS
 #%%
 sessionDict = {
-      'ses-1': '/media/Data/Lab_Projects/Aging/neuroimaging/raw_dicom/pb10520_levy',
-      # 'ses-2': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF020/RCF020_D2_tb1515_harpaz-rotem'
+      'ses-1': '/media/Drobo/Levy_Lab/Projects/PTSD_reconsolidation/TrioB/Scan_data/newer/RCF1099/RCF1099_D1_tb2387_harpaz-rotem'
+#      'ses-2': '/media/Data/Lab_Projects/KPE_PTSD_Project/neuroimaging/raw_dicom/kpe1480/kpe1480_scan2_pb8818_harpaz-rotem',
+#     'ses-3': '/media/Data/Lab_Projects/KPE_PTSD_Project/neuroimaging/raw_dicom/kpe1480/kpe1480_scan4_pb9370_harpaz-rotem'
         }
-subNumber = '20'
+subNumber = '1099'
 def fullBids(subNumber, sessionDict):
-    output_dir = '/media/Data/Lab_Projects/Aging/neuroimaging/Aging_BIDS/test'
+    output_dir = '/media/Data/Lab_Projects/RCF/neuroimaging/test'
     subName = 'sub-' + subNumber
     
     for i in sessionDict:
