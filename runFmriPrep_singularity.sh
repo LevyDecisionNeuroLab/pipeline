c307b32e4009837f1dc1a3adf882ff2879dffe80
orduek <orduek@gmail.com>
2020-06-08 12:44:21 -0400

    adeed version 20.0.6

--- runFmriPrep_singularity.sh
+++ runFmriPrep_singularity.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-#SBATCH -J fmriprep
-#SBATCH --array=1  # Replace indices with the right number of subjects
-#SBATCH --time=60:00:00
+#SBATCH -J fmriprepVer20_KPE
+#SBATCH --partition=general
+#SBATCH --array=2 # Replace indices with the right number of subjects
+#SBATCH --time=72:00:00
 #SBATCH -n 1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=4G
@@ -13,21 +14,21 @@
 #SBATCH --mail-type=ALL
 # ------------------------------------------
 
-SUBJ=(1307)
+SUBJ=(1263, 1286) #1223 1263 1293 1315 1356 1369 1468 1480 1499 1561)
 
-BIDS_DIR="/home/oad4/scratch60/KPE_BIDS" # change directory
-DERIVS_DIR="/derivatives" # the end point folder for fmriprep (should be in derivatives so don't touch unless you're know what yoou're doing)
-WORK_DIR="/home/oad4/scratch60/work_kpe" # enter working directory here
+BIDS_DIR="/home/oad4/scratch60/nf_bids" # change directory
+DERIVS_DIR="derivatives" # the end point folder for fmriprep (should be in derivatives so don't touch unless you're know what yoou're doing)
+WORK_DIR="/home/oad4/scratch60/work2" # enter working directory here
 
 mkdir -p $HOME/.cache/templateflow
-mkdir -p ${BIDS_DIR}/${DERIVS_DIR}
-mkdir -p ${BIDS_DIR}/derivatives/freesurfer-6.0.1
+#mkdir -p ${BIDS_DIR}/${DERIVS_DIR}
+#mkdir -p ${BIDS_DIR}/derivatives/freesurfer-6.0.1
 ln -s ${BIDS_DIR}/derivatives/freesurfer-6.0.1 ${BIDS_DIR}/${DERIVS_DIR}/freesurfer
 
 
 export SINGULARITYENV_FS_LICENSE=$HOME/pipeline/licenseFreeSurfer.txt # freesurfer license file
 export SINGULARITYENV_TEMPLATEFLOW_HOME="/templateflow"
-SINGULARITY_CMD="singularity run --cleanenv -B $HOME/.cache/templateflow:/templateflow -B ${WORK_DIR}:/work /project/ysm/levy_ifat/fmriPrep/fmriprep-1.5.8.simg"
+SINGULARITY_CMD="singularity run --cleanenv -B $HOME/.cache/templateflow:/templateflow -B ${WORK_DIR}:/work /project/levy_ifat/fmriPrep/fmriprep-20.0.6.simg"
 
 
 
